97e2f0529ab22f0b59e44abadc4f606d9893688f
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-10 14:02:54 -0300

    F310 - change style

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -9,6 +9,11 @@ class FullScreenMap extends StatefulWidget {
 
 class _FullScreenMapState extends State<FullScreenMap> {
   late MapboxMapController mapController;
+  final center = LatLng(37.82006418211845, -122.47861988235866);
+  String selectedStyle = 'mapbox://styles/bryan-pc/clcqh203r000b16pdsrarxzot';
+  final darkStyle = 'mapbox://styles/bryan-pc/clcqgz9fe001i14ohujgapkwo';
+  final lightStyle = 'mapbox://styles/bryan-pc/clcqh203r000b16pdsrarxzot';
+
 
   void _onMapCreated(MapboxMapController controller){
     mapController = controller;
@@ -17,14 +22,36 @@ class _FullScreenMapState extends State<FullScreenMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapboxMap(
-        accessToken: 'sk.eyJ1IjoiYnJ5YW4tcGMiLCJhIjoiY2xjcWZ5OTF5MDVibzNwbjJrMmlrazB5biJ9.HhKUC-9l69_6c-VZy1NRSg',
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: 
-        const CameraPosition(
-          target: LatLng(37.82006418211845, -122.47861988235866),
-          zoom: 14),
-      )
+      body: createMap(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.add_to_home_screen),
+            onPressed: (){
+              if (selectedStyle == darkStyle){
+                selectedStyle = lightStyle;
+              }else{
+                selectedStyle = darkStyle;
+              }
+
+              setState(() {});
+            }
+            )
+        ],
+      ),
+    );
+  }
+
+  MapboxMap createMap() {
+    return MapboxMap(
+      styleString: selectedStyle,
+      accessToken: 'sk.eyJ1IjoiYnJ5YW4tcGMiLCJhIjoiY2xjcWZ5OTF5MDVibzNwbjJrMmlrazB5biJ9.HhKUC-9l69_6c-VZy1NRSg',
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: 
+      CameraPosition(
+        target: center,
+        zoom: 14),
     );
   }
 }
